8f71c032aa8e86c7e4bd339c4d496406ef08cd65
0xd34df00d <0xd34df00d@gmail.com>
2018-05-19 18:07:42 -0400

    More uniform test structure.

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,13 +5,13 @@ import Data.MakeEnum.Options
 
 import Enum
 
+makeEnumWith ''OtherModuleEnum ['OMEUnknown]
+  defaultOptions { newEnumName = Just "OtherModuleEnumDerived" }
+
 data SameModuleEnum = SMEUnknown | SMEVar1 | SMEVar2 | SMEVar3 deriving (Eq, Ord, Show)
 
-makeEnumWith ''OtherModuleEnum ['OMEUnknown]
-  defaultOptions { newEnumName = Just "OtherDerivedEnum" }
-
 makeEnumWith ''SameModuleEnum ['SMEUnknown]
-  defaultOptions { newEnumName = Just "SameDerivedEnum", ctorNameModifier = ("Der" ++) }
+  defaultOptions { newEnumName = Just "SameModuleEnumDerived", ctorNameModifier = ("Der" ++) }
 
 main :: IO ()
 main = putStrLn "If it compiles, it works"
