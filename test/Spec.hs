f2d1f4187fdc9af1c2637b792177f330cd53901c
0xd34df00d <0xd34df00d@gmail.com>
2018-05-19 18:19:17 -0400

    Added a test for ADTs with fields.

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,5 +13,13 @@ data SameModuleEnum = SMEUnknown | SMEVar1 | SMEVar2 | SMEVar3 deriving (Eq, Ord
 makeEnumWith ''SameModuleEnum ['SMEUnknown]
   defaultOptions { newEnumName = Just "SameModuleEnumDerived", ctorNameModifier = ("Der" ++) }
 
+data EnumWithFields = EWFVar1 String
+                    | EWFVar2 Int Int
+                    | EWFUnknown
+                    deriving (Eq, Ord, Show)
+
+makeEnumWith ''EnumWithFields ['EWFUnknown]
+  defaultOptions { newEnumName = Just "EnumWithFieldsDerived", ctorNameModifier = ("Der" ++) }
+
 main :: IO ()
 main = putStrLn "If it compiles, it works"
