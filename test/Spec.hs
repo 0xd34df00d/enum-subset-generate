79b51d412c3a977e4dbc261915e96e6cb6d74777
0xd34df00d <0xd34df00d@gmail.com>
2018-05-19 00:18:14 -0400

    Added same module data test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,8 +4,13 @@ import Data.MakeEnum
 
 import Enum
 
+data SameModuleEnum = SMEUnknown | SMEVar1 | SMEVar2 | SMEVar3 deriving (Eq, Ord, Show)
+
 makeEnumWith ''OtherModuleEnum ['OMEUnknown]
   defaultOptions { newEnumName = Just "OtherDerivedEnum" }
+
+makeEnumWith ''SameModuleEnum ['SMEUnknown]
+  defaultOptions { newEnumName = Just "SameDerivedEnum", ctorNameModifier = ("Der" ++) }
 
 main :: IO ()
 main = putStrLn "If it compiles, it works"
