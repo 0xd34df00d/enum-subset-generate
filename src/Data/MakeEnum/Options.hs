7c5f00a9bdf99323d34f918000619eb2f54e88f7
0xd34df00d <0xd34df00d@gmail.com>
2018-05-24 00:32:25 -0400

    Added derivations list to options.

--- src/Data/MakeEnum/Options.hs
+++ src/Data/MakeEnum/Options.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Data.MakeEnum.Options(
     OptionsT(..),
     Options,
     defaultOptions
   ) where
 
+import Language.Haskell.TH
+
 data OptionsT f = Options
   { newEnumName :: f String
   , fromFunctionName :: f String
   , toFunctionName :: f String
   , ctorNameModifier :: String -> String
+  , deriveClasses :: [Name]
   }
 
 type Options = OptionsT Maybe
 
 defaultOptions :: Options
-defaultOptions = Options Nothing Nothing Nothing id
+defaultOptions = Options Nothing Nothing Nothing id [''Eq, ''Ord, ''Show]
